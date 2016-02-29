130d4cb09c520c28f8e49a2bffea9eaa5b992236
Nathan Wentworth <nwentworth95@gmail.com>
2016-02-29 16:52:00 -0500

    changed h1, h2 fonts, analysis

--- js/main.js
+++ js/main.js
@@ -7,22 +7,3 @@ $('nav li').hover(
     $('ul', this).stop().slideUp(100);
   }
 );
-
-for (var i = 1; i <= 12; i++) {
-	var path = document.querySelector('#stroke-' + i);
-	var length = path.getTotalLength();
-	// Clear any previous transition
-	path.style.transition = path.style.WebkitTransition =
-	  'none';
-	// Set up the starting positions
-	path.style.strokeDasharray = length + ' ' + length;
-	path.style.strokeDashoffset = length;
-	// Trigger a layout so styles are calculated & the browser
-	// picks up the starting position before animating
-	path.getBoundingClientRect();
-	// Define our transition
-	path.style.transition = path.style.WebkitTransition =
-	  'stroke-dashoffset 2s ease-in-out';
-	// Go!
-	path.style.strokeDashoffset = '0';
-}
